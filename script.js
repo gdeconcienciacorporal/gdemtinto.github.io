8f13aea2c760f9eb8643001c3d1062564bce953f
gdeconcienciacorporal <156134961+gdeconcienciacorporal@users.noreply.github.com>
2024-07-01 15:22:24 -0300

    Adding weeks 12 & 13

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
-  actualizarDiasRestantes();
-  setInterval(actualizarDiasRestantes, 24*60*60*1000); // Actualizar diario
+  //actualizarDiasRestantes();
+  //setInterval(actualizarDiasRestantes, 24*60*60*1000); // Actualizar diario
    // Datos para el gráfico de líneas
 const dataLine1 = {
   labels: ['Día 1', 'Día 2', 'Día 3', 'Día 4', 'Dia 5'], // Etiquetas de los días
   datasets: [{
-    label: 'Km recorridos en Semana 10',
-    data: [19, 13, 8, 7, 50], // Datos de los km recorridos en cada día
+    label: 'Km recorridos en Semana 12',
+    data: [8, 10, 11, 11, 25], // Datos de los km recorridos en cada día
     borderColor: 'rgba(75, 192, 192, 1)',
     fill: true,
     tension: 0.1
@@ -16,8 +16,8 @@ const dataLine1 = {
 const dataLine2 = {
   labels: ['Día 1', 'Día 2', 'Día 3', 'Día 4', 'Dia 5'], // Etiquetas de los días
   datasets: [{
-    label: 'Km recorridos en Semana 11',
-    data: [16, 16, 13, 23, 11], // Datos de los km recorridos en cada día
+    label: 'Km recorridos en Semana 13',
+    data: [5, 11, 11, 18, 15], // Datos de los km recorridos en cada día
     borderColor: 'rgba(153, 102, 255, 1)',
     fill: true,
     tension: 0.1
