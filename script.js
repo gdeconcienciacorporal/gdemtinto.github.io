48a7d3768169f06d674032c0668d136ce9b67fac
gdeconcienciacorporal <156134961+gdeconcienciacorporal@users.noreply.github.com>
2024-11-25 14:29:24 -0300

    Commit weeks 32 & 33

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ document.addEventListener("DOMContentLoaded", function () {
 const dataLine1 = {
   labels: ['Día 1', 'Día 2', 'Día 3', 'Día 4'], // Etiquetas de los días
   datasets: [{
-    label: 'Km recorridos en Semana 30',
-    data: [9, 9, 11, 16], // Datos de los km recorridos en cada día
+    label: 'Km recorridos en Semana 32',
+    data: [], // Datos de los km recorridos en cada día
     borderColor: 'rgba(75, 192, 192, 1)',
     fill: true,
     tension: 0.1
@@ -16,7 +16,7 @@ const dataLine1 = {
 const dataLine2 = {
   labels: ['Día 1', 'Día 2', 'Dia 3'], // Etiquetas de los días
   datasets: [{
-    label: 'Km recorridos en Semana 31',
+    label: 'Km recorridos en Semana 33',
     data: [7, 10, 42], // Datos de los km recorridos en cada día
     borderColor: 'rgba(153, 102, 255, 1)',
     fill: true,
